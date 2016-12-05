fa3f1bfda9c2fc2623aee8dab8895c5b2076ca7c
songchenwen <me@songchenwen.com>
2016-12-05 19:34:38 +0800

    Fix Callback

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ DHT_NODES = [['tracker.openbittorrent.com', 80],
              ['router.bittorrent.com', 6881], 
              ['dht.transmissionbt.com', 6881]];
 
+if (process.env.SPIDER_CALLBACK) {
+	console.log('callback ' + process.env.SPIDER_CALLBACK);
+}
+
 p2pspider({'port': process.env.SPIDER_PORT || 62333, 
 	'nodesMaxSize': 1000,
     'bootstrapNodes': DHT_NODES}, function(data){
     console.log(data);
 	if (process.env.SPIDER_CALLBACK) {
-		request.post('process.env.SPIDER_CALLBACK').form({info: JSON.stringify(data)});
+		request.post(process.env.SPIDER_CALLBACK).form({info: JSON.stringify(data)});
 	}
 });
